8048f8c35e82468c4adc91bddaef5291663dd33e
Quentin TREHEUX <quentin.treheux@epitech.eu>
2020-04-14 19:08:38 +0200

    [ADD] simpson method [FIX] compute

--- src/compute.c
+++ src/compute.c
@@ -28,7 +28,7 @@ static double borwein(double n, double x)
     double res2 = 0;
 
     if (x == 0)
-        return (0);
+        return (1);
     for (double k = 0; k <= n; k++) {
         temp = (2 * k) + 1;
         res1 = sin(x / temp);
@@ -74,11 +74,14 @@ void trapezoidal(double n, double pi)
 void simpson(double n, double pi)
 {
     double res = 0;
+    double temp = 0;
 
-    // suppress later
-    n = 0;
-    pi = 0;
-
+    for (double a = 0, b = 0.5; b <= 5000; a += 0.5, b += 0.5) {
+        temp = borwein(n, a) + borwein(n, b);
+        temp += 4 * borwein(n, ((a + b) / 2));
+        temp *= ((b - a) / 6);
+        res += temp;
+    }
     pi = diff_pi(pi, res);
     printf("Simpson:\nI%.0f = %.10f\n", n, res);
     printf("diff = %.10f\n", pi);
