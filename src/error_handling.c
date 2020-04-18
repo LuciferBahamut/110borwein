2da8a7cb578d343b0d995e82184b4644cf91356f
Chauvin Chloé <Chloe@localhost.localdomain>
2020-04-18 14:16:25 +0200

    [FIX] return of the write_error function [FIX] negative numbers

--- src/error_handling.c
+++ src/error_handling.c
@@ -13,13 +13,11 @@ int error_handling(char *str)
     int i = 0;
     int nbr = 0;
 
-    if (str[0] == '-')
-        i++;
     for (; str[i] != '\0'; i++) {
         if (str[i] >= '0' && str[i] <= '9')
             continue;
         else if (str[i] == '.') {
-            if (nbr == 0)
+            if (nbr != 0)
                 return (write_error(STR_ERROR_VALNBR));
             nbr++;
             continue;
